0058f383c08878be020de5d547a2925a65ace986
rahul kumar <rahul.javascript0@gmail.com>
2024-09-11 23:35:59 +0530

    change

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -6,7 +6,7 @@ export const POST = async (request) => {
   try {
     const body = await request.json();
     await sendMail(body);
-
+    console.log(body)
     return res.json({ success: true, message: body }, { status: 200 });
   } catch (err) {
     console.error("Error handling GET request:", err);
