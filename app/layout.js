27186a461dde98dbe980fb6e4d2d073d36564dc4
rahul kumar <rahul.javascript0@gmail.com>
2024-09-13 20:01:16 +0530

    favicon add

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,7 @@ import Image from "next/image";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Rahul Mern",
+  title: "Rahul Full Stack Developer",
   description: " MERN Stack Developer skilled in building scalable web apps with MongoDB, Express, React, and Node.js. Expert in API design, backend services, and responsive front-ends. Proficient in state  management, authentication, cloud deployments, and modern tools like TypeScript and Tailwind CSS. Strong problem-solver, team  player, and committed to delivering high-quality solutions.",
 };
 
