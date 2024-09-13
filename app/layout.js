767937c952b71ac4dc8ad70d83d1c002c14ba141
rahul kumar <rahul.javascript0@gmail.com>
2024-09-13 13:33:51 +0530

    title update

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,7 @@ import Image from "next/image";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Create Next App",
+  title: "Rahul Mern",
   description: " MERN Stack Developer skilled in building scalable web apps with MongoDB, Express, React, and Node.js. Expert in API design, backend services, and responsive front-ends. Proficient in state  management, authentication, cloud deployments, and modern tools like TypeScript and Tailwind CSS. Strong problem-solver, team  player, and committed to delivering high-quality solutions.",
 };
 
